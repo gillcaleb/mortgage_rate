b973979072dbebd7be65649b5fc25e5a66638049
gillcaleb <gillcmc21@gmail.com>
2023-03-17 16:01:59 -0400

    Update main.py
    fixing cors

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
 from bs4 import BeautifulSoup
 import requests
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/mortgage_rate")
 async def get_mortgage_rate():
